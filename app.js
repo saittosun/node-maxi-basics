e8535177146fa40a92b81b99f03e899d6160e8d9
tostos <saittosun2@gmail.com>
2020-08-05 09:26:27 +0200

    sending responses

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  // process.exit();
+  res.setHeader('Content-type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>my first page</title></head>');
+  res.write('<body><h1>hello</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
