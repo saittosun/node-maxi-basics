a147f0dca088db19f78106f4252be3ada0f1ed6b
tostos <saittosun2@gmail.com>
2020-08-05 10:05:30 +0200

    redirecting requests

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 // jshint esversion: 9
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url === '/') {// three equal sign anlami ikiside value ve string 
+  const method = req.method;
+  if (url === '/') {
     res.write('<html>');
     res.write('<head><title>enter message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();
   }
-  // yukaridaki kodu calistirdiktan sonra url '/message' olacagindan oturu asagidaki kod calisacak!!!
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();// burada ve yukarida return yazmak isek kod okuma durmaz asagiya dogru devam eder ve asagida write yazdigimiz icin hata verir
+  }
   res.setHeader('Content-type', 'text/html');
   res.write('<html>');
   res.write('<head><title>my first page</title></head>');
