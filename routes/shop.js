2092bb565794b4290fdd4b20628b4a7c04b6d789
tostos <saittosun2@gmail.com>
2021-02-21 21:40:52 +0100

    returning a error page

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
- res.send('<h1>hello from express!</h1>')
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')) //dirname is a global variable which simply holds the absolute path on our operating system  to this project folder
 })
 
 module.exports = router;
