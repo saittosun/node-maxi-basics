fbc13e0b2dbbf1fcdb3ac4c132d8b6259a580079
tostos <saittosun2@gmail.com>
2021-02-21 18:37:14 +0100

    not found page

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-// jshint esversion: 9
 const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
-// urlencoded does whole request body parsing. This parses bodies like the one we're writing here, sent through a form. {extended: false} should be able to parse non-default features.
-app.use(bodyParser.urlencoded({ extended: false }))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-  console.log('this is always run!');
-  next();
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('first middleware')
-  // if you are sending a response, this is a good indication that you never want to call next too because you don't want to execute any other response related code just as before with vanilla nodejs, you don't want to send more than one response, this won't work and will result in an error.
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">send</button></form>')
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-  console.log('second middleware')
-  res.end('<h1>hello from express!</h1>')
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
 })
 
 app.listen(3000)
