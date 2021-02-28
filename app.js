5b241691d6136c3568bcdec0deaf29ce0b183fc9
tostos <saittosun2@gmail.com>
2021-02-28 09:24:27 +0100

    sil

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs())
+app.set('view engine', 'hbs');
 app.set('views', 'views')
 
 const adminData = require('./routes/admin');
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,22 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-const adminData = require('./admin')
-const rootDir = require('../util/path');
-
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: "/" })
-})
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
 
 module.exports = router;
